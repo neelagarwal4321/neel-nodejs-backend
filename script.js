5937cb3f238f6f0d86ac22518b79f4be39fc00c2
neelagarwal4321 <neel0409@gmail.com>
2025-12-13 15:55:26 +0530

    commentsc update

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ app.get("/about", function(req, res){
 // this route will throw an error and go to the console / error handling middleware
 // and send a response to the client
 app.get("/profile", function(req, res, next){
-    return next(new Error("profile page not found"));
+    return next(new Error("profile page not found")); // message in the console
 });
 
 // error handling (always at the end) | the output is shown in the frotnend URL
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.status(500).send("Something broke!");
+    res.status(500).send("Something broke!"); // message in the frontend URL
 })
 
 app.listen(3000, function(){
