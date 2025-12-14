82d8fa87619633aa535792a81972634b9bd0b2bb
neelagarwal4321 <neel0409@gmail.com>
2025-12-14 11:39:03 +0530

    mastering ejs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function(req,res){
+    res.send("Chal rha hai...");
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
