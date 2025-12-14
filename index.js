ed42decf9d5696de57a52f6af76deb77fb8f59d6
neelagarwal4321 <neel0409@gmail.com>
2025-12-14 21:53:33 +0530

    commit bro stfu

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+// setting up parsers for form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs"); // rendering ejs files
+// setting up ejs as a view engine
+app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
+// creating routes
+app.get("/", function(req, res){
     res.send("Chal rha hai...");
-})
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
