b36276704e63b28fdc7bb345687553051846d2c0
neelagarwal4321 <neel0409@gmail.com>
2025-12-14 22:12:55 +0530

    ejs

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const app = express();
 // setting up parsers for form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 // setting up ejs as a view engine
 app.set("view engine", "ejs");
 
-// creating routes
+// creating routes using ejs files
 app.get("/", function(req, res){
-    res.send("Chal rha hai...");
+    res.render("index");
 });
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
-});
+}); 
