ff458f6ef24d1d6e30b72ce72917bc2911e6a1cc
Neel Agarwal <neel0409@gmail.com>
2025-12-16 15:52:05 +0530

    error-handling/create-user

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.get("/", function(req, res){
     res.render("index.ejs")
 });
 
+app.get("/profile", async(req, res) => {
+    try{
+        const user = {
+            id: 1,
+            name: "neel",
+            role: "admin"
+        }
+        if(!user){
+            res.status(404).json({success:false, message:"this profile was not file"});
+        }
+        res.status(200).json({success:true, data:user})
+    }
+});
+
+
 app.listen(3000, function(){
     console.log("server is running on port 3000.")
 })
