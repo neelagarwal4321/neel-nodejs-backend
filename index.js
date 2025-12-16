61d6fd7551982a3f101c31c05d4c13dd485a6f90
Neel Agarwal <neel0409@gmail.com>
2025-12-16 14:51:53 +0530

    commit msg

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const path = require('path');
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded( { extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.get("/", function(req, res){
-    res.render('index');
-})
+    res.render("index.ejs")
+});
 
 app.listen(3000, function(){
-    console.log("Server is running on 3000.")
+    console.log("server is running on port 3000.")
 })
