ae8191d4a3c9a9ee6984e36ab101088ac741ac54
Neel Agarwal <neel0409@gmail.com>
2025-12-16 16:38:23 +0530

    request.query-API

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// creating API
 app.get("/", function(req, res){
     res.render("index.ejs")
 });
@@ -24,7 +26,6 @@ app.get("/profile", async(req, res) => {
             role: "servant",
             age: 16
         }];
-
         if(!user){
             res.status(404).json({success:false, message:"this profile was not found.."});
         }
@@ -32,6 +33,36 @@ app.get("/profile", async(req, res) => {
     }
     catch(error){
         res.status(500).json({success:false, message:"internal server error..", error:error.message})
+    }
+});
+
+// creating another API
+app.get("/ages", async(req, res) => {
+    try{
+        const {age} = req.query;
+        const user = [{
+            id: 1,
+            name: "neel",
+            role: "admin",
+            age: 23
+        }, {
+            id: 2,
+            name: "jerry",
+            role: "cutie",
+            age: 16
+        }];
+        if(age){
+            if(Number(age)<18){
+                res.status(200).json({success: true, message: "success", data:user});
+            }
+            else{
+                res.status(403).json({success: false, message: "age must be greater than 18 bsdk.."});
+            }
+        }
+        res.status(200).json({success: true, message: "success", data:user});
+    }
+    catch(error){
+        res.status(500).json({success: false, message:"internal server error..", error:error.message});
     }
 });
 
