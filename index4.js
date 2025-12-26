09eb5955d3a7429278939412ed17cfad8c2576f9
Neel Agarwal <neel0409@gmail.com>
2025-12-26 11:56:12 +0530

    post

--- index4.js
+++ index4.js
@@ -20,5 +20,11 @@ app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
+app.get("/api/users/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id == id);
+    return res.json(user);
+});
+
 
 app.listen(PORT, () => console.log("The Server is running on port 8000."));
