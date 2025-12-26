9a95e53a3b5a6fc8ea399db22404d8427c9fa08b
Neel Agarwal <neel0409@gmail.com>
2025-12-26 12:38:53 +0530

    restful

--- index4.js
+++ index4.js
@@ -20,11 +20,25 @@ app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app.get("/api/users/:id", (req,res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id == id);
-    return res.json(user);
+app
+    .route("/api/users/:id")
+    .get((req,res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id == id);
+        return res.json(user);
+    })
+    .patch((req,res) => {
+        return res.json({status: "Pending"});
+    })
+    .delete((req,res) => {
+        return res.json({status: "Pending"});
+    });
+
+
+app.post("/api/users", (req,res) => {
+    return res.json({status: "Pending"});
 });
 
+// server
 
 app.listen(PORT, () => console.log("The Server is running on port 8000."));
