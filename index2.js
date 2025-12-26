61f21212b49c9966cb36f387d6f74c2fed7f04d5
Neel Agarwal <neel0409@gmail.com>
2025-12-26 11:55:45 +0530

    .route .post. get

--- index2.js
+++ index2.js
@@ -8,36 +8,35 @@ const user = [];
 
 // POST api
 
-app.post('/user', (req, res) => {
-    try{
-        const {job_title, salary} = req.body();
-        if(!String(job_title) && Number(salary)==undefined){
-            res.status(403).json({success:false, message:"Give a valid title and salary."});
+app
+    .route("/user")
+    .post((req, res) => {
+        try{
+            const {job_title, salary} = req.body();
+            if(!String(job_title) && Number(salary)==undefined){
+                res.status(403).json({success:false, message:"Give a valid title and salary."});
+            }
+            const new_user = [{
+                id: Date.now(),
+                name: "Neel",
+                job_title,
+                salary
+            }];
+            user.push(new_user);
+            res.status(200).json({success:true, message:"success", data:user});
         }
-        const new_user = [{
-            id: Date.now(),
-            name: "Neel",
-            job_title,
-            salary
-        }];
-        user.push(new_user);
-        res.status(200).json({success:true, message:"success", data:user});
-    }
-    catch(error){
-        res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
-    }
-});
-
-// GET Api
-
-app.get("/post", async(req, res) => {
-    try{
-        res.status(200).json({success:true, message:"success", data:user});
-    }
-    catch(error){
-        res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
-    }
-});
+        catch(error){
+            res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+        }
+    })
+    .get(async(req, res) => {
+        try{
+            res.status(200).json({success:true, message:"success", data:user});
+        }
+        catch(error){
+            res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+        }
+    });
 
 // server running
 
