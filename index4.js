492d6f7d1a9fe3ad1096b82b3d1fd45e70710a92
Neel Agarwal <neel0409@gmail.com>
2025-12-27 11:58:30 +0530

    middleware

--- index4.js
+++ index4.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 8000;
+
+// middleware
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    fs.appendFile(
+        "log2.txt",
+        `\n${Date.now()}: ${req.method}: ${req.path}\n`, (err, data) => {
+            next();
+    });
+});
 
 // route /users
 app.get("/users", (req, res) => {
