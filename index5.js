c95eb8603ba7e0b46b5d60187645aea9e78f4724
Neel Agarwal <neel0409@gmail.com>
2025-12-29 14:39:59 +0530

    delete API

--- index5.js
+++ index5.js
@@ -3,64 +3,108 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/fifawc", async(req,res) => {
+// get api practice
+
+app.get("/profile", async(req, res) => {
     try{
-        const fifa = [{
-            host: "South Africa",
-            year: 2010,
-            winner: "Spain"
+        const {id} = req.query;
+        const user = [{
+            name: "Cristiano",
+            age: 40,
+            id: 100
         }, {
-            host: "Brazil",
-            year: 2014,
-            winner: "Germany"
+            name: "Lionel",
+            age: 38,
+            id: 101
         }];
-        if(!fifa){
-            res.status(404).json({success:false, message:"404.. Profile Not Found"});
+        if(id){
+            if(Number(id)){
+                res.status(200).json({success:true, message:"success", data:user});
+            }
+            res.status(403).json({success:false, message:"must be a valiid ID (numeric)."});
         }
-        res.status(200).json({success:true, message:"Profile found.. success", data:fifa});
+        res.status(200).json({success:true, message:"success", data:user});
     }
     catch(error){
-        res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+        res.status(500).json({success:false, message:"internal server error.", error:error.message});
     }
 });
 
-const champions = [];
+// post api practice 
 
-app.post("/champ_league/winners", (req, res) => {
+app.post("/cart", (req,res) => {
     try{
-        const {host, winners, year} = req.body;
-        if(!String(host)){
-            res.status(403).json({success:false, message:"Give a valid host."});
+        const {item, qty} = req.body;
+        if(!String(item) && Number(qty)==undefined){
+            res.status(403).json({success:false, message:"must be a valid item and quantity."});
         }
-        else if(Number(year) == undefined){
-            res.status(403).json({success:false, message:"Give a valid year."});
-        }
-        else if(!String(winners)){
-            res.status(403).json({success:false, message:"Give a valid winner."});
-        }
-        const new_winners = [{
-            host,
-            winners,
-            year,
-            id: Date.now()
+        const cart = [{
+            name: "Riya",
+            item,
+            qty
         }];
-        champions.push(new_winners);
-        res.status(200).json({success:true, message:"success", data:champions});
+        res.status(200).json({success:true, message:"success", data:cart});
     }
     catch(error){
-        res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+        res.status(500).json({success:false, message:"internal server error.", error:error.message});
     }
 });
 
-app.get("/champ_league/winners", async(req, res) => {
+// REVIEW APIs creation (post and get)
+
+const review = [];
+
+app.post("/reviews", (req,res) => {
     try{
-        res.status(200).json({success:true, message:"success", data:champions});
+        const {movie_title, rating, review_text} = req.body;
+        if(!String(movie_title)){
+            return res.status(400).json({ success: false, message: "Movie title is required." });
+        }
+        else if(!review_text || typeof review_text !== 'string' || review_text.trim().length === 0){
+            res.status(403).json({success:false, message: "Review text is required." });
+        }
+        else if((Number(rating)!==0 && Number(rating)>5)){
+            res.status(403).json({success:false, message: "Rating must be between 1 and 5." });
+        }
+        const new_review = [{
+            user_id: Date.now(),
+            movie_title,
+            rating,
+            review_text,
+        }];
+        review.push(new_review);
+        res.status(200).json({success:true, message:"successfully given review", data:review});
     }
-    catch{
-        res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+    catch(error){
+        res.status(500).json({success:false, message:"Internal Server Error.", error:error.message});
     }
 });
 
-app.listen(8000, () => {
-    console.log("server running on port 8000...");
+app.delete("/reviews/:user_id", (req, res) => {
+    try{
+        const userId = Number(req.params.user_id);
+        const reviewIndex = review.findIndex(r => r.user_id === userId);
+
+        if(reviewIndex === -1){
+            return res.status(404).json({ success: false, message: "Review not found." });
+        }
+        review.splice(reviewIndex, 1);
+        res.status(200).json({ success: true, message: "Review deleted successfully" });
+    }
+    catch(error){
+        res.status(500).json({ success:true, message: "Internal Server Error..", error:error.message});
+    }
+});
+
+app.get("/reviews", async(req, res) => {
+    try{
+        res.status(200).json({success:true, message:"successfully given review", data:review});
+    }
+    catch(error){
+        res.status(500).json({success:false, message:"Internal Server Error.", error:error.message});
+    }
+});
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000.")
 });
