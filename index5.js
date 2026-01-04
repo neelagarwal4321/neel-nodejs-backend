4583bc4bded484134f8073d9fe43749283e66569
Neel Agarwal <neel0409@gmail.com>
2026-01-04 12:12:32 +0530

    status codes change

--- index5.js
+++ index5.js
@@ -95,13 +95,13 @@ app.delete("/reviews/:user_id", (req, res) => {
         const reviewIndex = review.findIndex(r => r.user_id === userId);
 
         if(reviewIndex === -1){
-            return res.status(404).json({ success: false, message: "Review not found." });
+            return res.status(404).json({success: false, message: "Review not found."});
         }
         review.splice(reviewIndex, 1);
-        res.status(200).json({ success: true, message: "Review deleted successfully" });
+        res.status(200).json({success: true, message: "Review deleted successfully"});
     }
     catch(error){
-        res.status(500).json({ success:true, message: "Internal Server Error..", error:error.message });
+        res.status(500).json({success:true, message: "Internal Server Error..", error:error.message});
     }
 });
 
