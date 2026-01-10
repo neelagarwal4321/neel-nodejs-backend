82e19d437673f4952b15bd818bd4469de9c58112
Neel Agarwal <neel0409@gmail.com>
2026-01-10 23:14:48 +0530

    post api electives route skeleton

---
+++ index6.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json);
+
+// restful api
+
+app.post("/electives", (req, res) => {
+    try{
+
+    }
+    catch(error){
+        
+    }
+});
