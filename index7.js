c11654d1397e75ae8a22c339fa06c76e7a253a39
Neel Agarwal <neel0409@gmail.com>
2026-01-17 14:22:06 +0530

    post api for the movies route

--- index7.js
+++ index7.js
@@ -35,10 +35,33 @@ app.get("/members", async(req, res) => {
 
 // restful api for the movies route
 
+const movies = [];
+
 // post api movies route
 
 app.post("/movies", (req, res) => {
-
+    try{
+        const {name,id,genre} = req.body;
+        if(!String(name) || typeof name !== 'string' || name.trim().length === 0){
+            res.status(400).json({success:false, message:"not a valid movie name"});
+        }
+        if(!String(id) || typeof id !== 'string' || id.trim().length === 0){
+            res.status(400).json({success:false, message:"not a valid movie id"});
+        }
+        if(!String(genre) || typeof genre !== 'string' || genre.trim().length === 0 || String(genre) !== "Action" || String(genre) !== "Comedy" || String(genre) !== "Horror" || String(genre) !== "Sci-Fi"){
+            res.status(400).json({success:false, message:"not a valid movie genre"});
+        }
+        const new_movie = [{
+            name,
+            id,
+            genre
+        }];
+        movies.push(new_movie);
+        return res.status(200).json({success:true, message:"The movie is added successfully", data:movies});
+    }
+    catch(error){
+        return res.status(400).json({success:false, message:"Internal Server Error", error:error.message});
+    }
 });
 
 // patch api movies route
