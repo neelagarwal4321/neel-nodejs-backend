73148071d9216774c7c1cf02c68e2d43c5b816a6
Neel Agarwal <neel0409@gmail.com>
2026-01-17 12:43:44 +0530

    get index7 skel

--- index7.js
+++ index7.js
@@ -1,1 +1,16 @@
 // complete practice
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/members", async(req, res) => {
+    try{
+
+    }
+    catch(error){
+        
+    }
+});
