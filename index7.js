aac1bba37e5dac8315e9a4eef0ee4861e5b98793
Neel Agarwal <neel0409@gmail.com>
2026-01-17 12:54:35 +0530

    get api for members route

--- index7.js
+++ index7.js
@@ -8,9 +8,28 @@ app.use(express.json());
 
 app.get("/members", async(req, res) => {
     try{
-
+        const {name} = req.query;
+        const member = [{
+            name: "Neel",
+            age: 23,
+            house: "Gryffindor"
+        }, {
+            name: "Riya",
+            age: "23",
+            house: "Slytherin"
+        }];
+        if(!member){
+            res.status(400).json({success:false, message:"the member is not found"});
+        }
+        if(name){
+            if(String(name) || typeof name === 'string'){
+                return res.status(200).json({success:true, message:"the member is present", data:member});
+            }
+        }
+        return res.status(200).json({success:true, message:"the member is present", data:member});
     }
     catch(error){
-        
+        return res.status(500).json({success:false, message:"Internal Server Error", data:error.message});
     }
 });
+
