cd71ccea2c8d446e31d35f211e545a0a7f03f9e7
Neel Agarwal <neel0409@gmail.com>
2026-01-28 13:08:25 +0530

    delete try catch

--- index8.js
+++ index8.js
@@ -37,7 +37,12 @@ app.put("/boxing", (req, res) => {
 // delete api
 
 app.delete("/boxing", (req, res) => {
+    try{
 
+    }
+    catch(error){
+        return res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
+    }
 });
 
 // get api
