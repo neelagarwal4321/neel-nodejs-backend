d4b7ae4fab251582d7dd05cf7337e7ba57e168c7
Neel Agarwal <neel0409@gmail.com>
2026-01-28 13:19:33 +0530

    post api complete

--- index8.js
+++ index8.js
@@ -6,11 +6,29 @@ app.use(express.json());
 
 // restful api for a single route
 
+const boxers = [];
+
 // post api
 
 app.post("/boxing", (req, res) => {
     try{
-
+        const {name, age} = req.body;
+        if(!String(name) || typeof name !== 'string' || name.trim().length === 0){
+            return res.status(404).json({success:false, message:"Give a valid name"});
+        }
+        if(!Number(age) || Number(age) === undefined){
+            return res.status(404).json({success:false, message:"Give a valid age"});
+        }
+        if(boxers.find((e) => e.id === id)){
+            return res.status(404).json({success:false, message:"Boxer already exists"});
+        }
+        const new_boxer = [{
+            name,
+            age,
+            id: Date.now()
+        }];
+        boxers.push(new_boxer);
+        return res.status(200).json({success:true, message:"The boxer is added successfully", data:boxers});
     }
     catch(error){
         return res.status(500).json({success:false, message:"Internal Server Error", error:error.message});
