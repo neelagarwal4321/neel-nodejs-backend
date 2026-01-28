d2ea0737cfe85702ce3e81d6372fb7affdc08186
Neel Agarwal <neel0409@gmail.com>
2026-01-28 12:58:42 +0530

    comments are ready for the restful api (basics)

---
+++ index8.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// restful api for a single route
+
+// post api
+
+// patch api
+
+// put api
+
+// delete api
+
+// get api
